80caf935ed5315208bb786120121a38058862e0b
demisforever <demisgero22@gmail.com>
2023-08-14 19:06:40 -0300

    client created

---
+++ client/react-filepr/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <p>Hola mundo cruel</p>
+
+        <div class="mb-3">
+          <input class="form-control form-control-sm" id="formFileSm" type="file" />
+          <button type="button" class="btn btn-success">Go!</button>
+        </div>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
